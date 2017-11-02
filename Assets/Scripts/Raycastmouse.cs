e9e6d34860f17c93c77aa720573fa2c62768b6b9
TomLelliott <thomaslel4@aol.com>
2017-11-02 16:43:53 +0000

    tidy up

---
+++ Assets/Scripts/Raycastmouse.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycastmouse : MonoBehaviour {
+
+
+		void FixedUpdate()
+		{
+			Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+			if (Physics.Raycast(transform.position, fwd, 10))
+				print("There is something in front of the object!");
+		}
+	}
